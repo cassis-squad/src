88fda3ad5d5f9d4b0d1f152c5b4d4e371f18b3b1
Filippo Ferrando Damillano <filippo.ferrando@itiscuneo.eu>
2022-03-08 22:05:06 +0100

    update

--- lib/wifi/wifiScan.py
+++ lib/wifi/wifiScan.py
@@ -11,23 +11,7 @@ logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
 class scan():
     def __init__(self):
-        self.networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
-        self.networks.set_index("BSSID", inplace=True)
-        logging.debug("Dataframe setup complete")
+       pass
     
     def callback(self, packet):
-        if packet.hasLayer(Dot11Beacon):
-            bssid = packet[Dot11].addr2
-            ssid = packet[Dot11Elt].info.decode()
-
-            try:
-                dbm_signal = packet.dBm_AntSignal
-                logging.debug("Gathered signal")
-            except:
-                dbm_signal = "N/A"
-                logging.debug("Ungathered signal")
-
-            stats = packet[Dot11Beacon].network_stats()
-            channel = stats.get("channel")
-            crypto = stats.get("crypto")
-            networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
+        pass
