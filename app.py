58cf3007aa521d7cf1adb97dc134e56d99b214a1
Filippo Ferrando Damillano <filippo.ferrando@itiscuneo.eu>
2022-03-09 21:44:01 +0100

    serialized ml model

--- app.py
+++ app.py
@@ -2,22 +2,12 @@ import logging
 
 from lib.arduino.commandToArduino import ArduinoCommands
 from lib.lidar.lidarObjectModel import *
-from lib.wifi.wifiScan import *
+from lib.wifi.wifiScan import scan
 
 # LOGGING PLATFORM
 LOGGING_FILE = "./log/log.log"
 logging.basicConfig(filename=LOGGING_FILE, encoding="utf-8", level=logging.DEBUG)
 
 
-class main:
-    def __init__(self):
-        self.arduino = ArduinoCommands()
-
-    def control(self):
-        usb = self.arduino.control()
-
-    def wifi(self):
-        pass
-
-
-m = main()
+while True:
+    pass
--- lib/wifi/wifiScan.py
+++ lib/wifi/wifiScan.py
@@ -2,6 +2,8 @@ import logging
 import pandas
 import time
 import os
+import pickle
+import numpy as np
 
 from scapy.all import *
 from threading import Thread
@@ -9,9 +11,59 @@ from threading import Thread
 
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
+#model
+model_filename = "wifi_model.pkl"
+with open(model_filename, "rb") as file:
+    wifi_m = pickle.load(file)
+
+interface = "wlan1mon"
+networks = {'itis-pvt',
+            'itis-wifi',
+            'wifi-itis',
+            'itis-pvt',
+            'itis-wifi2',
+            'AP_SMART25',
+            'wifi-lab01',
+            'AP_ITISLI03_2.5',
+            'AP_SMART50',
+            'AP_ITISLI03_5.0',
+            'AP_ITISLI02'}
+net_dict = {}
+
 class scan():
-    def __init__(self):
-        pass
-    
+    def __init__(self, destinazione):
+        self.destinazione = destinazione
+
     def callback(self, packet):
-        pass
+        if packet.haslayer(Dot11Beacon):
+            # get the name of it
+            ssid = packet[Dot11Elt].info.decode()
+            try:
+                dbm_signal = packet.dBm_AntSignal
+            except:
+                dbm_signal = "N/A"
+            if ssid in newtorks:
+                net_dict[ssid] = dbm_signal
+
+    def wifi(self):
+        global net_dict, interface, networks
+        net_dict = {i : None for i in networks}
+        sniff(prn=self.callback, iface=interface, count=100)
+        lan = np.array([[]])
+        for net in newtorks:
+            if net_dict[net] is None:
+                np.append(lan, "-120")
+            else: 
+                np.append(lan,net_dict[net])
+        
+        return lan
+
+    def wifiPositioning(self):
+        actual_scan = self.wifi()
+        actual_pos = wifi_m.predict(actual_scan)
+
+        if actual_pos == self.destinazione:
+            print("Sono arrivato")
+        else:
+            print("Non sono arrivato")
+        
